ef039e5b9400cfa6ee3d20b6f5e59bfad14be7b2
pangolin <eggkraken@gmail.com>
2026-02-08 23:27:48 -0800

    removed -cmi01a: Optimized timer usage and functions [Ryan Holtz]
    This change completely broke Mode 1. It's still a bit wonky but its a considerable improvement.

--- src/mame/fairlight/cmi01a.cpp
+++ src/mame/fairlight/cmi01a.cpp
@@ -9,7 +9,7 @@
 #include "emu.h"
 #include "cmi01a.h"
 
-#define VERBOSE     (0)
+#define VERBOSE     (1)
 #include "logmacro.h"
 
 DEFINE_DEVICE_TYPE(CMI01A_CHANNEL_CARD, cmi01a_device, "cmi_01a", "Fairlight CMI-01A Channel Card")
@@ -24,8 +24,8 @@ cmi01a_device::cmi01a_device(const machine_config &mconfig, const char *tag, dev
 	, m_sample_timer(nullptr)
 	, m_irq_cb(*this)
 	, m_current_sample(0), m_mosc(0.0), m_pitch(0), m_octave(0), m_zx_ff_clk(false), m_zx_ff(false), m_zx(false), m_gzx(false)
-	, m_run(false), m_not_rstb(true), m_not_load(false), m_not_wpe(true), m_new_addr(false)
-	, m_tri(false), m_permit_eload(false), m_not_eload(true), m_bcas_q1_enabled(true)
+	, m_run(false), m_not_rstb(true), m_not_load(false), m_not_zcint(true), m_not_wpe(true), m_new_addr(false)
+	, m_tri(false), m_permit_eload(false), m_not_eload(true), m_bcas_q1_enabled(true), m_bcas_q1(false), m_bcas_q2(false)
 	, m_env_dir(ENV_DIR_UP), m_env(0), m_env_divider(0), m_ediv_out(false), m_eclk(false), m_env_clk(false)
 	, m_wave_addr_lsb(0), m_wave_addr_msb(0), m_upper_wave_addr_load(false), m_wave_addr_msb_clock(true), m_run_load_xor(true), m_delayed_inverted_run_load(false)
 	, m_ptm_c1(false), m_ptm_o1(false), m_ptm_o2(false), m_ptm_o3(false)
@@ -71,6 +71,9 @@ void cmi01a_device::device_start()
 {
 	m_wave_ram = std::make_unique<u8[]>(0x4000);
 
+	m_bcas_q1_timer = timer_alloc(FUNC(cmi01a_device::bcas_q1_tick), this);
+	m_zcint_pulse_timer = timer_alloc(FUNC(cmi01a_device::zcint_pulse_cb), this);
+	m_rstb_pulse_timer = timer_alloc(FUNC(cmi01a_device::rstb_pulse_cb), this);
 	m_sample_timer = timer_alloc(FUNC(cmi01a_device::update_sample), this);
 
 	m_stream = stream_alloc(0, 1, 48000);
@@ -92,6 +95,7 @@ void cmi01a_device::device_start()
 	save_item(NAME(m_run));
 	save_item(NAME(m_not_rstb));
 	save_item(NAME(m_not_load));
+	save_item(NAME(m_not_zcint));
 	save_item(NAME(m_not_wpe));
 	save_item(NAME(m_new_addr));
 
@@ -100,6 +104,8 @@ void cmi01a_device::device_start()
 	save_item(NAME(m_not_eload));
 
 	save_item(NAME(m_bcas_q1_enabled));
+	save_item(NAME(m_bcas_q1));
+	save_item(NAME(m_bcas_q2));
 
 	save_item(NAME(m_env_dir));
 	save_item(NAME(m_env));
@@ -157,7 +163,8 @@ void cmi01a_device::device_reset()
 	m_ws = 0;
 	m_dir = 0;
 	m_env = 0;
-	m_bcas_q1_enabled = true;
+	m_bcas_q2 = false;
+	m_bcas_q1 = false;
 	m_not_rstb = true;
 
 	m_ptm_o1 = 0;
@@ -192,7 +199,10 @@ void cmi01a_device::device_reset()
 	m_kb0 = 1;
 	m_kb1 = 0;
 	m_kb2 = 0;
-
+    
+    m_bcas_q1_timer->adjust(attotime::from_hz(clock() / 2), 0, attotime::from_hz(clock() / 2));
+    m_zcint_pulse_timer->adjust(attotime::never);
+    m_rstb_pulse_timer->adjust(attotime::never);
 	m_sample_timer->adjust(attotime::never);
 
 	update_filters();
@@ -364,17 +374,36 @@ void cmi01a_device::set_run_load_xor(const bool run_load_xor)
 		return;
 
 	m_run_load_xor = run_load_xor;
-	m_new_addr = true;
+	if (m_rstb_pulse_timer->remaining().is_never())
+	{
+		m_rstb_pulse_timer->adjust(attotime::from_nsec(27500));
+		m_new_addr = true;
+	}
+	else
+	{
+		m_rstb_pulse_timer->adjust(attotime::never);
+	}
+	set_not_rstb(m_run_load_xor != m_delayed_inverted_run_load);
+}
 
-	// pulse /RSTB low
-	m_not_rstb = false;
-	set_gzx(true);
-	set_wave_addr_lsb(0);
-	set_wave_addr_msb(0x80 | m_ws);
+TIMER_CALLBACK_MEMBER(cmi01a_device::rstb_pulse_cb)
+{
+	m_delayed_inverted_run_load = !m_run_load_xor;
+	set_not_rstb(m_run_load_xor != m_delayed_inverted_run_load);
+}
 
-	// return /RSTB high
-	m_not_rstb = true;
-	set_gzx(false);
+void cmi01a_device::set_not_rstb(const bool not_rstb)
+{
+	if (not_rstb == m_not_rstb)
+		return;
+
+	m_not_rstb = not_rstb;
+	update_gzx();
+	if (!m_not_rstb)
+	{
+		set_wave_addr_lsb(0);
+		set_wave_addr_msb(0x80 | m_ws);
+	}
 }
 
 void cmi01a_device::update_bcas_q1_enable()
@@ -384,18 +413,24 @@ void cmi01a_device::update_bcas_q1_enable()
 
 	if (!old_enable && m_bcas_q1_enabled)
 	{
-		if (m_not_load)
-			m_ptm->set_ext_clock(0, clock() / 8.0);
-		else
-			m_ptm->set_ext_clock(0, 0.0);
-		m_ptm->set_ext_clock(1, clock() / 4.0);
-		m_ptm->set_ext_clock(2, clock() / 4.0);
+		m_bcas_q1_timer->adjust(attotime::from_hz(clock() / 2), 0, attotime::from_hz(clock() / 2));
 	}
 	else if (old_enable && !m_bcas_q1_enabled)
 	{
-		m_ptm->set_ext_clock(0, 0.0);
-		m_ptm->set_ext_clock(1, 0.0);
-		m_ptm->set_ext_clock(2, 0.0);
+		m_bcas_q1_timer->adjust(attotime::never);
+	}
+}
+
+TIMER_CALLBACK_MEMBER(cmi01a_device::bcas_q1_tick)
+{
+	const bool old_q1 = m_bcas_q1;
+	m_bcas_q1 = !m_bcas_q1;
+	m_ptm->set_c2(m_bcas_q1);
+	m_ptm->set_c3(m_bcas_q1);
+	if (old_q1 && !m_bcas_q1)
+	{
+		m_bcas_q2 = !m_bcas_q2;
+		update_ptm_c1();
 	}
 }
 
@@ -423,13 +458,23 @@ void cmi01a_device::set_zx_flipflop_state(const bool zx_ff)
 
 inline void cmi01a_device::pulse_zcint()
 {
-	// pulse /ZCINT low
-	m_pia[0]->ca1_w(1);
-	set_gzx(true);
+	set_not_zcint(false);
+	m_zcint_pulse_timer->adjust(attotime::from_nsec(2750));
+}
 
-	// return /ZCINT high
-	m_pia[0]->ca1_w(0);
-	set_gzx(false);
+TIMER_CALLBACK_MEMBER(cmi01a_device::zcint_pulse_cb)
+{
+	set_not_zcint(true);
+}
+
+void cmi01a_device::set_not_zcint(const bool not_zcint)
+{
+	if (not_zcint == m_not_zcint)
+		return;
+
+	m_not_zcint = not_zcint;
+	m_pia[0]->ca1_w(not_zcint);
+	update_gzx();
 }
 
 void cmi01a_device::set_not_load(const bool not_load)
@@ -442,6 +487,10 @@ void cmi01a_device::set_not_load(const bool not_load)
 	update_ptm_c1();
 }
 
+inline void cmi01a_device::update_gzx()
+{
+	set_gzx(!m_not_rstb || !m_not_zcint);
+}
 
 void cmi01a_device::set_gzx(const bool gzx)
 {
@@ -702,7 +751,7 @@ void cmi01a_device::set_zx(const bool zx)
 void cmi01a_device::update_ptm_c1()
 {
 	const bool old_ptm_c1 = m_ptm_c1;
-	m_ptm_c1 = !m_not_load && !m_zx;
+	m_ptm_c1 = (m_not_load && m_bcas_q2) || (!m_not_load && !m_zx);
 	if (old_ptm_c1 != m_ptm_c1)
 		m_ptm->set_c1(m_ptm_c1);
 }
@@ -734,6 +783,7 @@ int cmi01a_device::zx_r()
 {
 	return BIT(m_wave_addr_lsb, 6);
 }
+
 
 void cmi01a_device::write(offs_t offset, u8 data)
 {
